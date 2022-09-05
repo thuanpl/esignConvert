285fce46fe70d6d8f757fb9a359e682203ab62a1
thuanpl <thuanpl@vnpt.vn>
2022-09-05 15:23:07 +0700

    init commit

---
+++ src/main/java/com/example/demo/MainClass.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class MainClass{
+
+    /**
+     * Test Esignature
+     * ThuanPL
+     * @param args
+     */
+    public static void main(String[] args){
+
+
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/TestMainApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class TestMainApplication {
+
+    public static void main(String[] args) {
+
+//        try {
+//            InputStream templateInputStream = new FileInputStream("D:\\\\Research\\\\Test\\\\Sample.docx");
+//            WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(templateInputStream);
+//            MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
+//
+//            String outputfilepath = "D:\\\\Research\\\\Test\\\\Sample.pdf";
+//            FileOutputStream os = new FileOutputStream(outputfilepath);
+//            Docx4J.toPDF(wordMLPackage,os);
+//            os.flush();
+//            os.close();
+//        } catch (Throwable e) {
+//
+//            e.printStackTrace();
+//        }
+    }
+
+}
